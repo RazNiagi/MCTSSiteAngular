482e41556fa17193c9a227c6fc5fd4544a494456
RazNiagi <RazNiagi@gmail.com>
2025-12-02 10:21:46 -0500

    Updated home page to have cards for navigating to different routes using copilot, styled to be consistent with the other routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+interface RouteInfo {
+  path: string;
+  label: string;
+  icon: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  routes: RouteInfo[] = [
+    {
+      path: '/connect-four',
+      label: 'Connect Four',
+      icon: 'grid_on',
+      description: 'Play Connect Four against an AI opponent'
+    },
+    {
+      path: '/onitama',
+      label: 'Onitama',
+      icon: 'sports_esports',
+      description: 'Play Onitama against an AI opponent'
+    }
+  ];
 }
